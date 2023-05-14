5621182f00fd40df5ce3fd2725fdf2fa80c2efe4
Shoaib Huq <slimsho123@gmail.com>
2023-05-14 14:19:26 -0500

    added video subscriber

---
+++ Video/video_subsciber.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class VideoSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('video_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            'video',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.bridge = CvBridge()
+
+    def listener_callback(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        cv2.imshow('video', cv_image)
+        cv2.waitKey(1)  # Display the image for 1 ms
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    video_subscriber = VideoSubscriber()
+
+    rclpy.spin(video_subscriber)
+
+    video_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
